d5208b1d0a5e609ef3add53d72c21688874e9274
Bill Tiffany <daganlegalservice@gmail.com>
2021-10-21 13:15:57 -0600

    pushing styling

--- server/index.js
+++ server/index.js
@@ -7,6 +7,10 @@ app.get('/', function(req,res){
     res.sendFile(path.join(__dirname,"../index.html"))
 })
 
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/main.js'))
+  })
+
 const port = process.env.PORT || 4242;
 
 app.listen(port, ()=>console.log("The answer is 4242"));
